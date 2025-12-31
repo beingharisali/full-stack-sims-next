65ab777a8174ec974ad997a8d2854436a3baa89f
beingharisali <theharis99@gmail.com>
2025-12-31 17:02:13 +0500

    initial commit

---
+++ services/auth.api.ts
@@ -0,0 +1,39 @@
+import http from "./http";
+import { User } from "../types/user";
+
+export async function login(
+	email: string,
+	password: string
+): Promise<{ user: User; token: string }> {
+	const res = await http.post("/auth/login", { email, password });
+	return res.data;
+}
+export async function register(
+	firstName: string,
+	lastName: string,
+	email: string,
+	password: string,
+	role: string
+): Promise<{ user: User; token: string }> {
+	const res = await http.post("/auth/register", {
+		firstName,
+		lastName,
+		email,
+		password,
+		role,
+	});
+	return res.data;
+}
+
+// export async function getProfile(): Promise<{ user: User } | null> {
+//   try {
+//     const res = await http.get("/auth/profile");
+//     return res.data;
+//   } catch {
+//     return null;
+//   }
+// }
+
+export async function logoutApi(): Promise<void> {
+	localStorage.removeItem("token");
+}
