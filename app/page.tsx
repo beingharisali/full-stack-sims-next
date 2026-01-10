329be46556e5e6d382e3828c4e70912e6b379018
Rehan Ali <your-email@gmail.com>
2026-01-10 20:49:44 +0500

    Common Changes

--- app/page.tsx
+++ app/page.tsx
@@ -91,7 +91,7 @@ export default function LoginPage() {
               <option value="">Select Role</option>
               <option value="manager">Manager</option>
               <option value="admin">Admin</option>
-              <option value="sales">Sales</option>
+              <option value="sales">Saler</option>
             </select>
           </div>
 
