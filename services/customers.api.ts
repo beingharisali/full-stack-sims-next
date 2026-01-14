b4f0dd55e19ab135bac0279b1ff4251d712351d1
Rehan Ali <your-email@gmail.com>
2026-01-14 09:11:01 +0500

    cratecustomers

---
+++ services/customers.api.ts
@@ -0,0 +1,13 @@
+import http from "./http";
+import { Customers } from "@/types/customers";
+
+export async function createCustomers(
+    name: string,
+  city: string,
+  contactnumber: string,
+  status: string,
+ category: "wholesale" | "individual",
+):Promise<{customers:Customers}> {
+    const res =await http.post("/create",{name,city,contactnumber,status,category})
+    return res.data;
+}
