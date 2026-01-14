b8e682cbcfa6abe455aa2220e521413fe3f31e7c
Rehan Ali <your-email@gmail.com>
2026-01-14 09:46:35 +0500

    Upadte customer

--- services/customers.api.ts
+++ services/customers.api.ts
@@ -1,21 +1,32 @@
 import http from "./http";
 import { Customers } from "@/types/customers";
 
-export async function createCustomers(
+export async function createCustomer(
     name: string,
   city: string,
   contactnumber: string,
   status: string,
  category: "wholesale" | "individual",
-):Promise<{customers:Customers}> {
+):Promise<{customer:Customers}> {
     const res =await http.post("/create",{name,city,contactnumber,status,category})
     return res.data;
 }
-export async function getCustomers():Promise<{customers:Customers}> {
+export async function getCustomer():Promise<{customer:Customers}> {
 const res =await http.get("/get");
 return res.data    
 }
-export async function getSingleCustomers(id:string):Promise<{customers:Customers}>{
+export async function getSingleCustomer(id:string):Promise<{customer:Customers}>{
     const res =await http.get("/get/${id}");
     return res.data
+}
+export async function updateCustomer(
+      name: string,
+  city: string,
+  contactnumber: string,
+  status: string,
+ category: "wholesale" | "individual",
+ id:string
+):Promise<{customer:Customers}> {
+    const res=await http.put("/update/${id}",{name,city,contactnumber,status,category})
+return res.data    
 }
