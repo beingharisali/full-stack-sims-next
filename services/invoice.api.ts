1ebe131bef763848a81e525ea13cd5fac9544510
Rehan Ali <your-email@gmail.com>
2026-01-15 17:12:51 +0500

    create invoice

--- services/invoice.api.ts
+++ services/invoice.api.ts
@@ -1,40 +1,40 @@
-// import http from "./http";
-// import { Invoice } from "../types/invoice";
+import http from "./http";
+import { Invoice } from "../types/invoice";
 
-// export async function createInvoice(
-//   invoiceNumber: string,
-//   client: {
-//     name: string;
-//     email: string;
-//     phone: string;
-//     address: string;
-//   },
-//   items: {
-//     description: string;
-//     quantity: number;
-//     price: number;
-//     total: number;
-//   },
-//   subTotal: number,
-//   tax: number,
-//   grandTotal: number,
-//   status: "pending" | "paid" | "cancelled",
-//   invoiceDate: Date,
-//   dueDate: Date
-// ): Promise<{ invoice: Invoice }> {
-//   const res = await http.post("/", {
-//     invoiceNumber,
-//     client,
-//     items,
-//     subTotal,
-//     tax,
-//     grandTotal,
-//     status,
-//     invoiceDate,
-//     dueDate,
-//   });
-//   return res.data;
-// }
+export async function createInvoice(
+  invoiceNumber: string,
+  client: {
+    name: string;
+    email: string;
+    phone: string;
+    address: string;
+  },
+  items: {
+    description: string;
+    quantity: number;
+    price: number;
+    total: number;
+  },
+  subTotal: number,
+  tax: number,
+  grandTotal: number,
+  status: "pending" | "paid" | "cancelled",
+  invoiceDate: Date,
+  dueDate: Date
+): Promise<{ invoice: Invoice }> {
+  const res = await http.post("/", {
+    invoiceNumber,
+    client,
+    items,
+    subTotal,
+    tax,
+    grandTotal,
+    status,
+    invoiceDate,
+    dueDate,
+  });
+  return res.data;
+}
 
 // export async function getInvoices(): Promise<{ invoices: Invoice[] }> {
 //   const res = await http.get("/");
