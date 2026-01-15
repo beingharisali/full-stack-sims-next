7a0cf3dee205071b6005c1ec9203975b22f9b468
Rehan Ali <your-email@gmail.com>
2026-01-15 16:00:14 +0500

    create saler

--- services/sales.api.ts
+++ services/sales.api.ts
@@ -1,25 +1,25 @@
-// import http from "./http";
-// import { Saler } from "../types/sales";
+import http from "./http";
+import { Saler } from "../types/sales";
 
-// export async function createSaler(
-//   name: string,
-//   contactNumber: string, 
-//   category:
-//     |"mobile"
-//     |"laptop"
-//     | "headphones"
-//     | "tablet"
-//     | "televison"
-//     | "camera"
-//     | "smartwatch"
-//     | "accessories"
-//     | "home-appliances",
-//   status: string,
-//   orderitems: number,
-// ):Promise<{saler:Saler}> {
-//     const res =await http.post("/create",{name,orderitems,contactNumber,status,category})
-//     return res.data;
-// }
+export async function createSaler(
+  name: string,
+  contactNumber: string, 
+  category:
+    |"mobile"
+    |"laptop"
+    | "headphones"
+    | "tablet"
+    | "televison"
+    | "camera"
+    | "smartwatch"
+    | "accessories"
+    | "home-appliances",
+  status: string,
+  orderitems: number,
+):Promise<{saler:Saler}> {
+    const res =await http.post("/",{name,orderitems,contactNumber,status,category})
+    return res.data;
+}
 // export async function getSaler():Promise<{saler:Saler[]}> {
 // const res =await http.get("/get");
 // return res.data    
