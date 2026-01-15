5e2e0321d154a204969f7f3a749f72a466be4949
Rehan Ali <your-email@gmail.com>
2026-01-15 17:19:38 +0500

    Update invoice

--- services/invoice.api.ts
+++ services/invoice.api.ts
@@ -48,41 +48,41 @@ export async function getSingleInvoice(
   return res.data;
 }
 
-// export async function updateInvoice(
-//   id: string,
-//   invoiceNumber: string,
-//   client: {
-//     name: string;
-//     email: string;
-//     phone: string;
-//     address: string;
-//   },
-//   items: {
-//     description: string;
-//     quantity: number;
-//     price: number;
-//     total: number;
-//   }[],
-//   subTotal: number,
-//   tax: number,
-//   grandTotal: number,
-//   status: "pending" | "paid" | "cancelled",
-//   invoiceDate: Date,
-//   dueDate: Date
-// ): Promise<{ invoice: Invoice }> {
-//   const res = await http.put(`/${id}`, {
-//     invoiceNumber,
-//     client,
-//     items,
-//     subTotal,
-//     tax,
-//     grandTotal,
-//     status,
-//     invoiceDate,
-//     dueDate,
-//   });
-//   return res.data;
-// }
+export async function updateInvoice(
+  id: string,
+  invoiceNumber: string,
+  client: {
+    name: string;
+    email: string;
+    phone: string;
+    address: string;
+  },
+  items: {
+    description: string;
+    quantity: number;
+    price: number;
+    total: number;
+  }[],
+  subTotal: number,
+  tax: number,
+  grandTotal: number,
+  status: "pending" | "paid" | "cancelled",
+  invoiceDate: Date,
+  dueDate: Date
+): Promise<{ invoice: Invoice }> {
+  const res = await http.put(`/${id}`, {
+    invoiceNumber,
+    client,
+    items,
+    subTotal,
+    tax,
+    grandTotal,
+    status,
+    invoiceDate,
+    dueDate,
+  });
+  return res.data;
+}
 
 // export async function deleteInvoice(
 //   id: string
