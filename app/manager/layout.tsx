1b53c23ac772629f38de79becd40bb4ece6318b3
Bareera Nawaz <bareeranawaz1043@gmail.com>
2026-01-18 14:23:45 +0500

    task completed

--- app/manager/layout.tsx
+++ app/manager/layout.tsx
@@ -1,4 +1,5 @@
-import ProtectedRoute from "@/components/protectedroutes";
+"use client";
+import ProtectedRoute from "@/app/components/protectedroutes";
 
 export default function ManagerLayout({ children }: { children: React.ReactNode }) {
   return <ProtectedRoute allowedRoles={["admin", "manager"]}>{children}</ProtectedRoute>;
